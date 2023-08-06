1572ecbe1604f20b55edb1c20e2976683a4f61a9
Mostafa Hassan <mostafa.hassan@iucon.com.eg>
2023-08-06 18:46:20 +0300

    SMTP by using Mailtrap

---
+++ SMTP_Mailtrap/SMTP_Mailtrap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("78239281a333cf", "4bc96c7b2d8fb6"),
+                EnableSsl = true
+            };
+            client.Send("from@example.com", "to@example.com", "Hello world11", "testbody");
+            Console.WriteLine("Sent");
+            Console.ReadLine();
+        }
+    }
+}
